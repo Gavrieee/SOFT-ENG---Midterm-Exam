2f121044a21fab2ce642c4250c55becb97f05241
Gavrieee <167282522+Gavrieee@users.noreply.github.com>
2024-10-29 20:43:26 +0800

    register.php

--- register.php
+++ register.php
@@ -46,7 +46,7 @@ require_once 'core/handleForms.php';
 		}
 
 		input[type="text"],
-		input[type="password"] {
+		input[type="password"], input[type="tel"], select {
 			width: 100%;
 			box-sizing: border-box;
 			text-align: left;
@@ -64,14 +64,32 @@ require_once 'core/handleForms.php';
 				<p style="color: red; font-size: 14px; text-align: center;"><i><?php echo $_SESSION['message']; ?></i></p>
 			<?php }
 			unset($_SESSION['message']); ?>
+
 			<form action="core/handleForms.php" method="POST">
 				<p>
-					<label style="font-weight: bold;" for="username">Username</label>
+					<label style="font-weight: bold;" for="username">Name</label>
 					<input type="text" name="username" required>
 				</p>
 				<p>
 					<label style="font-weight: bold;" for="username">Password</label>
 					<input type="password" name="password" required>
+				</p>
+				<p>
+					<label style="font-weight: bold;" for="username">Library Staff ID</label>
+					<input type="text" name="library_staff_id" required>
+					
+				</p>
+				<p>
+					<label style="font-weight: bold;" for="username">Contact Number</label>
+					<input type="tel" name="contact_number" pattern="^0\d{3}-\d{3}-\d{4}$" title="e.g., 0912-345-6789" required>
+				</p>
+				<p>
+					<label style="font-weight: bold;" for="username">Employment Type</label>
+					<select name="employment_type" id="employment_type" value="Select an option here">
+						<option value="full-time">Full-Time</option>
+						<option value="part-time">Part-Time</option>
+						<option value="contract">Contract</option>
+					</select>
 				</p>
 				<p style="text-align: center;">
 					<input style="font-size: 16px;" type="submit" name="registerUserBtn" value="Register">
